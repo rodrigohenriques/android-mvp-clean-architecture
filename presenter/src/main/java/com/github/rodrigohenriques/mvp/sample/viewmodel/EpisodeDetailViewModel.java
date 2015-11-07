0d09108e57e97999366751b1afff5fd5bac3d392
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-11-07 08:35:44 -0200

    add databinding dependencies

---
+++ presenter/src/main/java/com/github/rodrigohenriques/mvp/sample/viewmodel/EpisodeDetailViewModel.java
@@ -0,0 +1,11 @@
+package com.github.rodrigohenriques.mvp.sample.viewmodel;
+
+public class EpisodeDetailViewModel {
+    public final String duration;
+    public final String plot;
+
+    public EpisodeDetailViewModel(String duration, String plot) {
+        this.duration = duration;
+        this.plot = plot;
+    }
+}
